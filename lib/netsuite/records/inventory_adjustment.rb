7df4b18d3dffa03a2b8b8c5579f099db43fc3340
John Greiner <jgreiner@spoonflower.com>
2016-09-27 11:32:42 +0000

    Create inventory adjustment record and tests.

---
+++ lib/netsuite/records/inventory_adjustment.rb
@@ -0,0 +1,46 @@
+module NetSuite
+  module Records
+    class InventoryAdjustment
+      include Support::Fields
+      include Support::RecordRefs
+      include Support::Records
+      include Support::Actions
+      include Namespaces::TranInvt
+
+      actions :get, :add, :update, :upsert, :delete, :search
+
+      # https://system.netsuite.com/help/helpcenter/en_US/srbrowser/Browser2014_1/schema/record/inventoryadjustment.html
+      
+      fields :created_date, :last_modified_date, :memo, :tran_date, :tran_id 
+
+      field :custom_field_list, CustomFieldList
+      #field :inventorylist, InventoryAdjustmentInventoryList
+
+      # TODO from standard copied item record
+      # field :accounting_book_detail_list,  AccountingBookDetailList
+      
+      read_only_fields :estimated_total_value
+
+      record_refs :account, :adj_location, :customer, :custom_form, :department, :klass, :location,
+        :posting_period, :subsidiary
+
+      attr_reader   :internal_id
+      attr_accessor :external_id
+      attr_accessor :search_joins
+
+      def initialize(attributes = {})
+        @internal_id = attributes.delete(:internal_id) || attributes.delete(:@internal_id)
+        @external_id = attributes.delete(:external_id) || attributes.delete(:@external_id)
+        initialize_from_attributes_hash(attributes)
+      end
+
+      def to_record
+        rec = super
+        if rec["#{record_namespace}:customFieldList"]
+          rec["#{record_namespace}:customFieldList!"] = rec.delete("#{record_namespace}:customFieldList")
+        end
+        rec
+      end
+    end
+  end
+end
---
+++ spec/netsuite/records/inventory_adjustment_spec.rb
@@ -0,0 +1,65 @@
+require 'spec_helper'
+
+describe NetSuite::Records::InventoryAdjustment do
+  let(:inventory_adjustment) { NetSuite::Records::InventoryAdjustment.new }
+  
+  it 'has all the right read_only_fields' do
+    [
+      :estimated_total_value
+    ].each do |field|
+      expect(NetSuite::Records::InventoryAdjustment).to have_read_only_field(field)
+    end
+  end
+  
+  it 'has all the right attributes' do
+    [
+      :created_date, :last_modified_date, :memo, :tran_date, :tran_id 
+    ].each do |field|
+      expect(inventory_adjustment).to have_field(field)
+    end
+  end
+
+  it 'has all the right record refs' do
+    [
+      :account, :adj_location, :customer, :custom_form, :department, :klass, :location,
+        :posting_period, :subsidiary
+    ].each do |record_ref|
+      expect(inventory_adjustment).to have_record_ref(record_ref)
+    end
+  end
+
+  describe '#location_list' do
+    it 'can be set from attributes'
+  end
+
+  describe '#custom_field_list' do
+    it 'can be set from attributes'
+    it 'can be set from a CustomFieldList object'
+  end
+
+  describe '.get' do
+    context 'when the response is successful' do
+      let(:response) { NetSuite::Response.new(:success => true, :body => { :memo => 'This is a test' }) }
+
+      it 'returns a inventory_adjustment instance populated with the data from the response object' do
+        expect(NetSuite::Actions::Get).to receive(:call).with([NetSuite::Records::InventoryAdjustment, {:external_id => 1}], {}).and_return(response)
+        inventory_adjustment = NetSuite::Records::InventoryAdjustment.get(:external_id => 1)
+        expect(inventory_adjustment).to be_kind_of(NetSuite::Records::InventoryAdjustment)
+        expect(inventory_adjustment.memo).to eql('This is a test')
+      end
+    end
+
+    context 'when the response is unsuccessful' do
+      let(:response) { NetSuite::Response.new(:success => false, :body => {}) }
+
+      it 'raises a RecordNotFound exception' do
+        expect(NetSuite::Actions::Get).to receive(:call).with([NetSuite::Records::InventoryAdjustment, {:external_id => 1}], {}).and_return(response)
+        expect {
+          NetSuite::Records::InventoryAdjustment.get(:external_id => 1)
+        }.to raise_error(NetSuite::RecordNotFound,
+          /NetSuite::Records::InventoryAdjustment with OPTIONS=(.*) could not be found/)
+      end
+    end
+  end
+
+end
