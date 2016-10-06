117ee92ead23c5491ff2acaa92a07578dd05d219
John Greiner <jgreiner@spoonflower.com>
2016-10-06 18:06:34 +0000

    Add method to inventory adjustment inventory.

--- lib/netsuite/records/inventory_adjustment_inventory.rb
+++ lib/netsuite/records/inventory_adjustment_inventory.rb
@@ -16,8 +16,25 @@ module NetSuite
 
       record_refs :item, :location, :units
 
-      def initialize(attributes = {})
-        initialize_from_attributes_hash(attributes)
+      def initialize(attributes_or_record = {})
+        case attributes_or_record
+        when Hash
+          initialize_from_attributes_hash(attributes_or_record)
+        when self.class
+          initialize_from_record(attributes_or_record)
+        end
+      end
+
+      def initialize_from_record(record)
+        self.attributes = record.send(:attributes)
+      end
+
+      def to_record
+        rec = super
+        if rec["#{record_namespace}:customFieldList"]
+          rec["#{record_namespace}:customFieldList!"] = rec.delete("#{record_namespace}:customFieldList")
+        end
+        rec
       end
     end
   end
