ef71179292be3e7f6da4d2d01629e0a3f88b0609
Kristen Frey <kfrey@spoonflower.com>
2016-10-25 19:53:17 +0000

    FA-17 #comment custom record type upsert broken

--- lib/netsuite/records/custom_record_type.rb
+++ lib/netsuite/records/custom_record_type.rb
@@ -3,7 +3,9 @@ module NetSuite
     class CustomRecordType
       include Support::Fields
       include Support::RecordRefs
+      include Support::Records
       include Support::Actions
+      include Namespaces::SetupCustom
 
       actions :get, :get_list, :add, :delete, :upsert
 
@@ -17,11 +19,11 @@ module NetSuite
       record_ref :owner
 
       attr_reader :internal_id
-      attr_accessor :external_id
+      #attr_accessor :external_id
 
       def initialize(attributes = {})
         @internal_id = attributes.delete(:internal_id) || attributes.delete(:@internal_id)
-        @external_id = attributes.delete(:external_id) || attributes.delete(:@external_id)
+        #@external_id = attributes.delete(:external_id) || attributes.delete(:@external_id)
         initialize_from_attributes_hash(attributes)
       end
 
